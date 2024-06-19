5f6e8031b00b2a456baff320bb869921c9ee0fc7
Pancrass Njoroge <wankenobi29@gmail.com>
2024-06-19 13:09:14 +0300

    imported gsap to gsap.js

--- javascript/gsap.js
+++ javascript/gsap.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-/*import {gsap} from "./gsap/gsap.min.js";
+import {gsap} from "./gsap/gsap.min.js";
 /**Event Listener for mouseMove Event
  * Gets the current mouse position and on mouseMove event, updates the cursor position
  */
@@ -46,155 +45,6 @@ window.addEventListener('mousemove', e => {
 
  
 
-=======
-gsap.set(".cursor",{xPercent:-50, yPercent:-50})
-
-let cursor = document.querySelector(".cursor")
-
-let mouseX;
-let mouseY;
-
-window.addEventListener('mousemove', e => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-
-    gsap.to(cursor, 2, {x: mouseX, y: mouseY})
-});
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    let activeItemIndicator = CSSRulePlugin.getRule(".menu-item p#active::after");
-    const toggleButton = document.querySelector(".burger");
-    let isOpen = false;
-
-    gsap.set(".menu-item p", {y: 225});
-
-
-    const timeline = gsap.timeline({paused: true});
-
-    timeline.to(".overlay", {
-        duration: 1.5,
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-        ease: "power4.inOut"
-    });
-
-    timeline.to(".menu-item p", {
-        duration: 1.5,
-        y: 0,
-        stagger: 0.3,
-        ease: "power4.inOut"
-    }, "-=1");
-
-    timeline.to(activeItemIndicator, {
-        width: "100%",
-        duration: 1,
-        ease: "power4.inOut",
-        delay: 0.5
-    }, "<");
-
-    timeline.to(".sub-nav", {
-        bottom: "10%",
-        opacity: 1,
-        duration: 0.5,
-        delay: 0.5
-    }, "<");
-    
-
-    toggleButton.addEventListener("click", function() {
-        if(isOpen) {
-            timeline.reverse();
-        }else {
-            timeline.play();
-        }
-        isOpen = !isOpen;
-    })
-})
-
-/*
-
-gsap.registerPlugin(ScrollTrigger);
-
-const tl4 = gsap.timeline({ paused:true });
-let path = document.querySelector("path");
-let spanBefore = CSSRulePlugin.getRule("#hamburger span:before");
-
-gsap.set(spanBefore, {background:"#000"});
-gsap.set(".menu", {visibility:"hidden"});
-
-//toggles menu
- function revealMenu() {
-   revealMenuItems();
-
-   const hamburger = document.getElementById("hamburger");
-   const toggleBtn = document.getElementById("toggle-btn");
-
-   toggleBtn.onclick = function (e) {
-       hamburger.classList.toggle("active");
-       tl4.reversed(!tl4.reversed());
-   };
- }
-
- revealMenu();
-
- function revealMenuItems() {
-   const start = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
-   const end = "M0,1005S175,995,500,995s500,5,500,5V0H0Z";
-
-   const power2 = "power2.inOut";
-   const power4 = "power4.inOut"; 
-
-   tl4.to("#hamburger", 1.25, {
-       marginTop: "-5px",
-       x: -40,
-       y: 40,
-       ease: power4,
-   });
-
-   tl4.to("#hamburger span", 1, {
-       background: "#e2e2dc",
-       ease: power2,
-   },"<");
-
-   tl4.to(spanBefore, 1, {
-       background: "#e2e2dc",
-       ease: power2,
-   },"<");
-
-   tl4.to(".btn .btn-outline", 1.25,{
-       x: -30,
-       y: 30,
-       width: "20px",
-       height: "20px",
-       border: "1px solid #e2e2dc",
-       ease: power4,
-   }, "<");
-
-tl4.to(path, 0.8, {
-   attr: {
-       d: start,
-   },
-   ease: power2.easeIn,
-}, "<").to(path, 0.8, {
-   attr: {
-       d: end,
-   },
-   ease: power2.easeIn,
-}, "-0.5"); 
-
-tl4.to(".menu", 1, {
-   visibility: "visible",
-},"-=0.5");
-
-tl4.to(".menu-item > a", 1, {
-   top: 0,
-   ease: "power3.out",
-   stagger: {
-       amount: 0.5 
-   }
-},"-=1").reverse();
-
- }
->>>>>>> 945dfad5b5fca4eeb73411a0df85928e5ab7451e
 
 
 
@@ -326,13 +176,8 @@ class App {
 
             tl.to(image, {
                 ease: "none",
-<<<<<<< HEAD
                 yPercent: gsap.utils.random(-50, -100) + (index * 12), // Adjust parallax effect based on index
                 duration: .4 // Duration of the parallax effect
-=======
-                yPercent: gsap.utils.random(-60, -120) + (index * 10), // Adjust parallax effect based on index
-                duration: 1 // Duration of the parallax effect
->>>>>>> 945dfad5b5fca4eeb73411a0df85928e5ab7451e
             }, 0);
         });
     }
@@ -393,28 +238,3 @@ const heroPage = document.querySelector(".hero");
         currentScroll = window.pageYOffset;
     });
 
-<<<<<<< HEAD
-=======
-/*
-const sectionScroll = document.querySelector(".hero");
-let sectionScrollWidth = sectionScroll.offsetWidth;
-let amountToScroll = sectionScrollWidth - window.innerWidth;
-
-const tween = gsap.to(sectionScroll, {
-    x: - amountToScroll,
-    duration: 3,
-    ease: "none"
-    
-});
-
-ScrollTrigger.create({
-    trigger:".move_righter",
-    start:"top top",
-    end:"+=" + amountToScroll,
-    pin: true,
-    Animation:tween,
-    scrub:1,
-    markers:true
-})
-*/
->>>>>>> 945dfad5b5fca4eeb73411a0df85928e5ab7451e
